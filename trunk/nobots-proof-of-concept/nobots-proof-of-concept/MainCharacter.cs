bfd87ca402929c1c1353aa436150eb54b4b8faf2
karolina.baltyn@gmail.com <karolina.baltyn@gmail.com@b15a2b35-ca87-4338-f3a2-592455313c67>
2012-02-22 16:39:43 +0000

    Added Character.cs and MainCharacter.cs

---
+++ trunk/nobots-proof-of-concept/nobots-proof-of-concept/MainCharacter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace nobots_proof_of_concept
+{
+    class MainCharacter : Character
+    {
+        private MainCharacter instance;
+
+        private MainCharacter(Game game) : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
+        public MainCharacter Instance()
+        {
+            if(instance == null)
+                lock(this)
+                {
+                    if(instance == null)
+                    {
+                        instance = new MainCharacter(Game);
+                    }
+                }
+            return instance;
+        }
+    }
+}
