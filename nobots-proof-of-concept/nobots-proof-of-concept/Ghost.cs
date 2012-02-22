266c246cd1776a42414143d899490efdd7fef624
karolina.baltyn@gmail.com <karolina.baltyn@gmail.com@b15a2b35-ca87-4338-f3a2-592455313c67>
2012-02-22 18:03:10 +0000

    Added Element.cs, Mouse.cs, Ghost.cs and changed Game1.cs (added first fields and methods)

---
+++ nobots-proof-of-concept/nobots-proof-of-concept/Ghost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace nobots_proof_of_concept
+{
+    class Ghost : Character
+    {
+        public Ghost(Game game)
+            : base(game)
+        {
+        }
+    }
+}
