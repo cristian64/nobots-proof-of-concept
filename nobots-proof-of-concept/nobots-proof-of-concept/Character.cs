a7c1d1061d9cada6cdcd1529bb2b3fb3b7fe14f4
karolina.baltyn@gmail.com <karolina.baltyn@gmail.com@b15a2b35-ca87-4338-f3a2-592455313c67>
2012-02-22 16:39:43 +0000

    Added Character.cs and MainCharacter.cs

---
+++ nobots-proof-of-concept/nobots-proof-of-concept/Character.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace nobots_proof_of_concept
+{
+    class Character : DrawableGameComponent
+    {
+        public Vector2 position;
+        public Vector2 direction;
+        public int width;
+        public int height;
+        public int stepSize;
+        public float rotation;
+
+        public bool isHaunted;
+
+        public List<Texture2D> textures;
+
+        public Character(Game game) : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
